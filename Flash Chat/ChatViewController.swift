9b8be1870f01e9f90d396965f9cae42aa0abc2e4
Serena Cheng <serena.ch1999@gmail.com>
2019-07-12 13:59:32 -0400

    Implement textfield methods and save messages to Firebase.

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     // Declare instance variables here
     var messageArray = ["message 1", "message 2", "message 3"]
@@ -28,15 +28,17 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         messageTableView.dataSource = self
         
         //TODO: Set yourself as the delegate of the text field here:
-
-        
+        messageTextfield.delegate = self
         
         //TODO: Set the tapGesture here:
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
         
+        messageTableView.addGestureRecognizer(tapGesture)
         
-
         //TODO: Register your MessageCell.xib file here:
          messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        
+        configureTableView()
         
     }
 
@@ -63,8 +65,9 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     //TODO: Declare tableViewTapped here:
-    
-    
+    @objc func tableViewTapped() {
+        messageTextfield.endEditing(true)
+    }
     
     //TODO: Declare configureTableView here:
     func configureTableView() {
@@ -78,39 +81,59 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     //MARK:- TextField Delegate Methods
     
-    
-
-    
     //TODO: Declare textFieldDidBeginEditing here:
-    
-    
-    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.25) {
+            self.heightConstraint.constant = 360
+            self.view.layoutIfNeeded()
+        }
+        
+    }
     
     //TODO: Declare textFieldDidEndEditing here:
-    
-
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.25) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+        
+    }
     
     ///////////////////////////////////////////
     
     
     //MARK: - Send & Recieve from Firebase
     
-    
-    
-    
-    
     @IBAction func sendPressed(_ sender: AnyObject) {
         
+        messageTextfield.endEditing(true)
         
         //TODO: Send the message to Firebase and save it in our database
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
         
+        let messageDB = Database.database().reference().child("Messages")
+        let messageDict = ["Sender" : Auth.auth().currentUser?.email, "MessageBody" : messageTextfield.text!]
+        
+        messageDB.childByAutoId().setValue(messageDict) { (error, reference) in
+            
+            if error != nil {
+                print(error!)
+            }
+            else {
+                print("Message saved successfully")
+            }
+            
+            self.messageTextfield.isEnabled = true
+            self.sendButton.isEnabled = true
+            self.messageTextfield.text = ""
+        }
         
     }
     
     //TODO: Create the retrieveMessages method here:
-    
-    
-
     
     
     
